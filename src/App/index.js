cf7881a98d04efbb9c6e4e69dc146ffa6e8ae8ac
seungkyu2 <zx6658@naver.com>
2018-06-27 20:20:02 +0900

    scss loader 적용

--- src/App/index.js
+++ src/App/index.js
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
+import styles from "./index.scss";
+import classNames from "classnames/bind";
 
+const cx = classNames.bind(styles);
+const moduleName = "App";
 class App extends Component {
   render() {
-    return <div>app</div>;
+    return <div className={cx(`${moduleName}`)}>app</div>;
   }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,42 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.(js|jsx)$/, loader: "babel-loader", exclude: /node_modules/ }
+      { test: /\.(js|jsx)$/, loader: "babel-loader", exclude: /node_modules/ },
+
+      {
+        test: /\.css$/,
+        use: [
+          require.resolve("style-loader"),
+          {
+            loader: require.resolve("css-loader"),
+            options: {
+              modules: true,
+              localIdentName: "[path][name]__[local]--[hash:base64:5]",
+              importLoaders: 1
+            }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          require.resolve("style-loader"),
+          {
+            loader: require.resolve("css-loader"),
+            options: {
+              importLoaders: 1,
+              modules: true,
+              localIdentName: "[name]__[local]__[hash:base64:5]"
+            }
+          },
+          {
+            loader: require.resolve("sass-loader"),
+            options: {
+              includePaths: [path.resolve("src/scss")]
+            }
+          }
+        ]
+      }
     ]
   },
   resolve: {
